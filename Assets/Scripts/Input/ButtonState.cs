62e1294a85386723b45976df61e0e32d4a41826d
Ellandq <waselland@gmail.com>
2025-04-24 22:55:54 +0200

    Updated MouseInput component

--- Assets/Scripts/Input/ButtonState.cs
+++ Assets/Scripts/Input/ButtonState.cs
@@ -2,54 +2,10 @@
 
 namespace Input
 {
-    public class ButtonState : IEquatable<ButtonState>, IComparable<ButtonState>
+    public enum ButtonState
     {
-        public static readonly ButtonState UP = new ButtonState("UP");
-        public static readonly ButtonState DOWN = new ButtonState("DOWN");
-        public static readonly ButtonState HELD = new ButtonState("HELD");
-
-        public string Name { get; }
-        public int HeldForFrames { get; set; }
-
-        private ButtonState(string name)
-        {
-            Name = name;
-            HeldForFrames = 0;
-        }
-
-        public override string ToString() => Name;
-        
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as ButtonState);
-        }
-
-        public bool Equals(ButtonState other)
-        {
-            return other != null && Name == other.Name;
-        }
-
-        public override int GetHashCode()
-        {
-            return Name.GetHashCode();
-        }
-        
-        public int CompareTo(ButtonState other)
-        {
-            return other == null ? 1 : string.Compare(Name, other.Name, StringComparison.Ordinal);
-        }
-        
-        public static bool operator ==(ButtonState left, ButtonState right)
-        {
-            if (ReferenceEquals(left, right)) return true;
-            if (left is null || right is null) return false;
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(ButtonState left, ButtonState right)
-        {
-            return !(left == right);
-        }
+        Up,
+        Down
     }
 
 }
--- Assets/Scripts/Input/MouseInput.cs
+++ Assets/Scripts/Input/MouseInput.cs
@@ -24,5 +24,47 @@ namespace Input
             UpdateButtonStateDictionary();
             UpdateEventDictionaries();
         }
+        
+        private void Update ()
+        {
+            foreach (var button in _buttonAssignment)
+            {
+                var previousState = _buttonStates[button.Value];
+
+                _buttonStates[button.Value] = UnityEngine.Input.GetMouseButton(button.Value);
+
+                switch (_buttonStates[button.Value])
+                {
+                    case true when !previousState:
+                        _onInputAction[button.Key]?.Invoke(ButtonState.Down);
+                        break;
+                    case false when previousState:
+                        _onInputAction[button.Key]?.Invoke(ButtonState.Up);
+                        break;
+                }
+            }
+        } 
+        
+        private void UpdateButtonStateDictionary () 
+        {
+            _buttonStates = new Dictionary<int, bool>();
+
+            foreach (var button in _buttonAssignment){
+                _buttonStates.Add(button.Value, false);
+            }
+        }
+
+        private void UpdateEventDictionaries () 
+        {
+            _onInputAction = new Dictionary<string, Action<ButtonState>>();
+
+            foreach (var tuple in InputManager.MouseButtons){
+                _onInputAction.Add(tuple.action, (state) => {});
+            }
+        }
+        
+        public bool GetButtonState (int key){
+            return _buttonStates[key];
+        }
     }
 }
